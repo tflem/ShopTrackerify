5a91a1dbd0cf64132283ed74ee0f892d5146f9d2
tfleming <tfleming26@hotmail.com>
2018-05-01 11:09:58 -0600

    Update db context with product db set

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ShopTrackerify.Models;
 
 namespace ShopTrackerify.Data
 {
@@ -14,9 +11,12 @@ namespace ShopTrackerify.Data
         {
         }
 
+        public DbSet<Product> Products { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            builder.Entity<Product>().ToTable("Products");
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
