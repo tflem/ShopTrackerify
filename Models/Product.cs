c5dc684cc61dd7a7aca82788aec0c151bc54cfc7
tfleming <tfleming26@hotmail.com>
2018-05-01 11:08:35 -0600

    Add models folder with product class

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopTrackerify.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
