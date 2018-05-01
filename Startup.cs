c3a48a77094901da415d0bbf6b723799682d4266
tfleming <tfleming26@hotmail.com>
2018-05-01 11:12:58 -0600

    Clean up usings

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ShopTrackerify.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ShopTrackerify.Data;
 using ShopTrackerify.Services;
 
 namespace ShopTrackerify
